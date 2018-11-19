9c25739771bdf9ba195b6acdfabc18f474c3a370
kamanzidanny <44752252+kamanzidanny@users.noreply.github.com>
2018-11-19 15:14:14 +0200

    Update parcels.js

--- api/routes/parcels.js
+++ api/routes/parcels.js
@@ -1,17 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const parcels=[];
+parcels.push({
+		id: '1',
+		from: 'usa',
+		to:'china',
+		date: '12/11/2018',
+		sender:'aliver',
+		userId:'11',
+		receiver:'kev',
+		type:'transit',
+		status: 'pending'
+	});
+parcels.push({
+		id: '2',
+		from: 'kigali',
+		to:'musanze',
+		date: '11/11/2018',
+		userId:'12',
+		sender:'Danny',
+		receiver:'Eric',
+		type:'express',
+		status: 'pending'
+	})
 
 router.get('/',(req,res,next) =>{
-	res.status(200).json({
-		message: 'handling GET requests to /parcels'
-	});
+	res.status(200).json(parcels);
 });
 
 router.post('/',(req,res,next) =>{
+	const { from,to,sender,receiver,type}=req.body;
 	const parcel = {
-		name: req.body.name,
-		price: req.body.price
+		from,
+		to,
+		sender,
+		receiver,
+		type,
+		id:(parcels.length+1).toString(),
+		date:new Date(),
+		status: 'pending'
 	};
+	parcels.push(parcel);
 	res.status(201).json({
 		message: 'handling POST requests to /parcels!',
 		createdParcel: parcel
@@ -19,52 +48,54 @@ router.post('/',(req,res,next) =>{
 });
 
 router.get('/:parcelId',(req,res,next) =>{
-	const id = req.params.parcelId;
-	const from = req.params.parcelfrom;
-	const to = req.params.parcelto;
-	const date = req.params.parcelDate;
-	const sender = req.params.parcelsender;
-	const receiver = req.params.parcelreceiver;
-	const type = req.params.parceltype;
-	if(id === '1'){
-	res.status(200).json({
-		message: 'you discovered a special Id',
-		id: id,
-		from: 'usa',
-		to:'china',
-		date: '12/11/2018',
-		sender:'aliver',
-		receiver:'kev',
-		type:'transit'
-	});
-   } 
-   if(id === '2'){
-	res.status(200).json({
-		message: 'this is your discovered a special Id',
-		id: id,
-		from: 'kigali',
-		to:'musanze',
-		date: '11/11/2018',
-		sender:'Danny',
-		receiver:'Eric',
-		type:'express'
-	});
-   }
-   else {
-   	res.status(200).json({
-   		message: 'you passed an ID'
-   	});
-   }
-});
+	let id = req.params.parcelId;
+	id= id.toString()
+	const check= parcels.find(parcel => parcel.id===id)
+	if(check===undefined){
+		res.status(404).json({message: 'parcel not found'});
+	} else {
+		res.status(200).json(check);
+	}
+})
 
-router.patch('/:productId', (req, res, next) =>{
+router.patch('/:parcelId', (req, res, next) =>{
+	const{ receiver,to,type}=req.body;
+	const id  = req.params.parcelId;
+	parcel=parcels.find( parcel => parcel.id === id);
+	const index=parcels.indexOf(parcel);
+	parcels[index].receiver=receiver || parcels[index].receiver;
+	parcels[index].to=to || parcels[index].to;
+	parcels[index].type=type || parcels[index].type;
 	res.status(200).json({
-		message: 'updated parcel!'
+		message: 'updated parcel!',
+		updatedParcel: parcels[index]
 	});
 });
-router.delete('/:productId', (req, res, next) =>{
+router.delete('/:parcelId', (req, res, next) =>{
+	const id  = req.params.parcelId;
+	parcel=parcels.find( parcel => parcel.id === id);
+	const index=parcels.indexOf(parcel);
+    parcels.splice(index,1);
 	res.status(200).json({
-		message: 'deleted parcel!'
+		message: 'Parcel was deleted'
 	});
+});
+router.put('/:parcelId', (req, res, next) =>{
+    const id  = req.params.parcelId;
+    parcel=parcels.find( parcel => parcel.id === id);
+    const index=parcels.indexOf(parcel);
+    parcels[index].status="Canceled";
+    res.status(200).json({
+	message: 'Canceled Parcel',
+	canceleParcel:parcels[index]
+});
+});
+router.get('users/:userId/parcels', (req, res, next) =>{
+    const userId  = req.params.userId;
+    const parcelsforUser=parcels.filter( parcel => parcel.userId === userId);
+    res.status(200).json({
+	message: 'Parcels of user',
+	parcels:parcelsforUser
+});
 });
 module.exports = router;
